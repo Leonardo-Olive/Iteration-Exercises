e7dbe4b463a584feaf2e230ada9f8eb424403e32
leonardo oliveira <leonardo_olivesilva@icloud.com>
2022-11-30 18:49:43 -0800

    First commit

---
+++ Iteration-Exercise1/Iteration-Exercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Iteration_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+            //for (int i = 0; i <15; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
+
